56cc37215d04052a958b9585eacf32b2ddcdf9d0
kannan <kannans89@hotmail.com>
2025-10-31 22:04:30 +0530

    Add user interaction feature: prompt for name and personalized greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+GreetUser(name);
+
+static void GreetUser(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
